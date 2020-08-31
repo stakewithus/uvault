2f686aef9f8eaeb22ca493893e47a5a94e388aea
taz <tsk.nakamura@gmail.com>
2020-08-31 12:46:04 +0900

    rename ERC20 mock

--- tests/unit/Controller/test_controller_deposit.py
+++ tests/unit/Controller/test_controller_deposit.py
@@ -4,23 +4,23 @@ import brownie
 ZERO_ADDRESS = "0x0000000000000000000000000000000000000000"
 
 
-def test_deposit(accounts, controller, mockStrategy, mockERC20):
+def test_deposit(accounts, controller, mockStrategy, erc20):
     # mock vault address
     vault = accounts[1]
     controller.setStrategy(vault, mockStrategy, {'from': accounts[0]})
 
     amount = 1000
-    mockERC20.mint(vault, amount)
-    mockERC20.approve(controller, amount, {'from': vault})
+    erc20.mint(vault, amount)
+    erc20.approve(controller, amount, {'from': vault})
 
     balanceBefore = {
-        "vault": mockERC20.balanceOf(vault)
+        "vault": erc20.balanceOf(vault)
     }
 
     controller.deposit(amount, {'from': vault})
 
     balanceAfter = {
-        "vault": mockERC20.balanceOf(vault)
+        "vault": erc20.balanceOf(vault)
     }
 
     # check deposit was called with amount
@@ -37,4 +37,3 @@ def test_deposit_not_vault(accounts, controller):
 def test_deposit_strategy_zero_address(accounts, controller):
     # strategy cannot be set to zero address
     pass
-
--- tests/unit/Controller/test_controller_withdraw.py
+++ tests/unit/Controller/test_controller_withdraw.py
@@ -4,7 +4,7 @@ import brownie
 ZERO_ADDRESS = "0x0000000000000000000000000000000000000000"
 
 
-def test_withdraw(accounts, controller, mockStrategy, mockERC20):
+def test_withdraw(accounts, controller, mockStrategy, erc20):
     # mock vault address
     vault = accounts[1]
     controller.setStrategy(vault, mockStrategy, {'from': accounts[0]})
@@ -15,8 +15,8 @@ def test_withdraw(accounts, controller, mockStrategy, mockERC20):
 
     # check deposit was called with amount
     assert mockStrategy.withdrawAmount() == amount
-    # cannot test vault's balance of mockERC20 since mockStrategy
-    # does not transfer mockERC20
+    # cannot test vault's balance of erc20 since mockStrategy
+    # does not transfer erc20
 
 
 def test_withdraw_not_vault(accounts, controller):
@@ -28,4 +28,3 @@ def test_withdraw_not_vault(accounts, controller):
 def test_withdraw_strategy_zero_address(accounts, controller):
     # strategy cannot be set to zero address
     pass
-
