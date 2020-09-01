9aef25ca25594fdb093a7e250ed123e17c3ac15c
taz <tsk.nakamura@gmail.com>
2020-09-02 08:21:03 +0900

    StrategyYVault: fix test get balance

--- tests/unit/StrategyYVault/test_strategy_get_balance.py
+++ tests/unit/StrategyYVault/test_strategy_get_balance.py
@@ -2,7 +2,7 @@ import brownie
 
 
 def test_get_balance(accounts, strategyYVault, mockYCRV, mockYVault):
-    mockYVault.mintTo(strategyYVault, 200)
+    mockYVault.setBalanceOf(strategyYVault, 200)
     mockYCRV.transfer(strategyYVault, 100, {'from': accounts[0]})
 
     assert strategyYVault.getBalance() == 300
