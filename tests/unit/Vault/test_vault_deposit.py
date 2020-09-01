414ab232fa195808023d3728e77c1290a3f0f4a1
taz <tsk.nakamura@gmail.com>
2020-09-01 09:00:30 +0900

    Vault: property based test deposit

--- tests/unit/Vault/test_vault_deposit.py
+++ tests/unit/Vault/test_vault_deposit.py
@@ -1,12 +1,14 @@
 import brownie
+from brownie.test import given, strategy
 
 ZERO_ADDRESS = "0x0000000000000000000000000000000000000000"
 
 
-def test_deposit_total_supply_is_zero(accounts, vault, erc20):
-    sender = accounts[0]
-    amount = 1000
-
+@given(
+    sender=strategy('address'),
+    amount=strategy('uint256')
+)
+def test_deposit(accounts, vault, erc20, sender, amount):
     # mint ERC20 to sender, approve vault to spend
     erc20.mint(sender, amount)
     erc20.approve(vault, amount, {'from': sender})
