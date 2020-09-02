441f4d05a781593669954a9513ae256d3b371e7c
taz <tsk.nakamura@gmail.com>
2020-09-02 16:00:58 +0900

    script to deploy to dev

--- scripts/depoly_dev.py
+++ scripts/depoly_dev.py
@@ -11,15 +11,15 @@ def main():
     admin = accounts[0]
     treasury = accounts[1]
     pool = accounts[2]  # pool used by strategy
+    # tx should be sent from gas relayer,
+    # but use this address in case you want to quickly test
+    relayer = accounts[3]
 
     print("Admin:", admin.address)
     print("ETH:", admin.balance())
 
     # deploy ERC20 representing a valuable token such as DAI, USDC, etc...
     erc20 = ERC20.deploy("DAI", "DAI", 18, 0, {'from': admin})
-    # mint to accounts
-    for account in accounts:
-        erc20.mint(account, 1000 * 10 ** 18, {'from': admin})
 
     # Deploy gas token
     chiToken = ChiToken.deploy({'from': admin})
@@ -41,3 +41,43 @@ def main():
 
     # set strategy
     controller.setStrategy(vault, strategy, {'from': admin})
+
+    # ----------------------------------------------------------------------
+    # test gas costs
+
+    BATCH_SIZE = 1000
+    NUM_SIGNERS = 100
+
+    # add signers
+    signers = []
+    for i in range(NUM_SIGNERS):
+        accounts.add()
+        signers.append(accounts[-1])
+
+    # mint ERC20
+    for signer in signers:
+        erc20.mint(signer, 100 * 10 ** 18)
+
+    amount = 100 * 10 ** 18
+
+    # approve vault to deposit
+    for signer in signers:
+        erc20.approve(vault, amount, {'from': signer})
+
+    # test batch deposit
+    _accounts = []
+    amounts = []
+
+    for i in range(BATCH_SIZE):
+        if i < len(signers):
+            _accounts.append(signers[i])
+            amounts.append(amount)
+        else:
+            _accounts.append(ZERO_ADDRESS)
+            amounts.append(0)
+
+    # call batchDeposit through gas relayer
+    call_data = vault.batchDeposit.encode_input(_accounts, amounts)
+
+    gasRelayer.mintGasToken(100)
+    gasRelayer.relayTx(100, vault, call_data)
