54de5afe20551de2c017bfe9e2ef0b7e6ad53797
taz <tsk.nakamura@gmail.com>
2020-09-08 14:13:33 +0900

    fix yVault interface

--- contracts/strategies/interfaces/yVault.sol
+++ contracts/strategies/interfaces/yVault.sol
@@ -2,6 +2,7 @@ pragma solidity ^0.6.0;
 
 // https://github.com/iearn-finance/vaults/blob/master/contracts/yVault.sol
 interface yVault {
+  function balanceOf(address) external view returns (uint);
   function deposit(uint256 _amount) external;
   function withdraw(uint256 _amount) external;
 }
