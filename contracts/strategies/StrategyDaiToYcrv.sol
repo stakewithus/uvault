b97477afb60ceeb6398e084a6703125e381af588
taz <tsk.nakamura@gmail.com>
2020-09-09 11:22:35 +0900

    rename StrategyDaiToYcrv

--- contracts/strategies/StrategyDaiToYcrv.sol
+++ contracts/strategies/StrategyDaiToYcrv.sol
@@ -17,7 +17,7 @@ import "../interfaces/IVault.sol";
 
 // TODO interface IStrategy
 // TODO: claim all CRV to DAI and withdraw to vault
-contract StrategyDaiToCrv {
+contract StrategyDaiToYcrv {
     using SafeERC20 for IERC20;
     using SafeMath for uint256;
 
