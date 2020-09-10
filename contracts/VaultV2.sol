b9da81947795399bf5ec509945ff2c71eaaa7344
taz <tsk.nakamura@gmail.com>
2020-09-10 16:48:15 +0900

    Vault interface

--- contracts/VaultV2.sol
+++ contracts/VaultV2.sol
@@ -16,18 +16,18 @@ import "./interfaces/IVault.sol";
 // TODO: reentrancy lock
 // TODO circuit breaker
 // TODO: keep percentage of underlying token as reserve to save gas on withdraw
+// TODO inline safeTransfer to save gas?
+// TODO  protect agains hack by directly sending token to this contract's address
+// TODO: implement reserve to make withdraw cheap
+// TODO: safe withdraw any token in case strategy sends back wrong token
 
 contract VaultV2 is IVault, ERC20 {
     using SafeERC20 for IERC20;
     using SafeMath for uint;
 
-    event Invest(uint amount);
-    event Deposit(address indexed from, uint amount);
-    event Withdraw(address indexed to, uint amount);
-
     address public admin;
-    address public override token;
-    address public override strategy;
+    address override public token;
+    address override public strategy;
 
     constructor(
         address _token, string memory _name, string memory _symbol
@@ -55,42 +55,31 @@ contract VaultV2 is IVault, ERC20 {
 
     function setStrategy(address _strategy) override public onlyAdmin {
         require(_strategy != address(0)); // dev: strategy == zero address
-        require(IStrategy(_strategy).token() == token); // dev: strategy.token != vault.token
+        require(IStrategy(_strategy).underlyingToken() == token); // dev: strategy.token != vault.token
         require(IStrategy(_strategy).vault() == address(this)); // dev: strategy.vault != vault
         require(_strategy != strategy); // dev: new strategy == current strategy
 
         // withdraw from current strategy
         if (strategy != address(0)) {
             IERC20(token).safeApprove(strategy, 0);
-
-            uint bal = IStrategy(strategy).balance();
-            if (bal > 0) {
-                IStrategy(strategy).withdraw(bal);
-            }
+            IStrategy(strategy).exit();
         }
 
         strategy = _strategy;
-        // NOTE: Many ERC20s require approval from zero to nonzero or nonzero to zero
-        IERC20(token).safeApprove(strategy, 0);
         IERC20(token).safeApprove(strategy, uint256(-1));
     }
 
-    function _balance() internal view returns (uint) {
-        // NOTE: total balance = balance of vault + strategy
-        return IERC20(token).balanceOf(address(this)).add(IStrategy(strategy).balance());
-    }
-
-    function balance() override external view returns (uint) {
-        return _balance();
-    }
-
-    function invest() override external whenStrategyDefined {
-        uint bal = IERC20(token).balanceOf(address(this));
-
-        if (bal > 0) {
-            IStrategy(strategy).deposit(bal);
-            emit Invest(bal);
-        }
+    /*
+    @notice Invest token in vault into strategy
+    @param _amount Amount of token to invest
+    @param _min Min amount of yield earning tokens to return. Prevents slippage
+    */
+    function invest(uint _amount, uint _min)
+        override external onlyAdmin whenStrategyDefined
+    {
+        require(_amount > 0); // dev: amount = 0
+        // NOTE: infinite approval is set when this strategy was set
+        IStrategy(strategy).deposit(_amount, _min);
     }
 
     function deposit(uint _amount) override external {
@@ -98,30 +87,42 @@ contract VaultV2 is IVault, ERC20 {
 
         _mint(msg.sender, _amount);
         IERC20(token).safeTransferFrom(msg.sender, address(this), _amount);
-
-        emit Deposit(msg.sender, _amount);
     }
 
-    function withdraw(uint _shares, uint _min) override external whenStrategyDefined {
+    /*
+    @notice Withdraw shares for yield token
+    @param _shares Amount of shares to burn
+    @param _min Minimum number of yield token to return
+    */
+    function withdraw(uint _shares, uint _min)
+        override external whenStrategyDefined
+    {
         uint totalSupply = totalSupply();
         require(totalSupply > 0); // dev: total supply == 0
         require(_shares > 0); // dev: amount == 0
 
         /*
-        s = shares
+        s = sahres
         T = total supply of shares
-        a = amount of tokens
-        B = total balance of tokens in vault + strategy
-        s / T = a / B
-        a = s * B / T
-        */
-        uint amount = _balance().mul(_shares).div(totalSupply);
-        uint bal = IERC20(token).balanceOf(address(this));
+        y = amount of yield token to return
+        Y = total amount of yield token
 
+        s / T = y / Y
+
+        NOTE: value of y per s is low when there are many token in vault
+              not yet invested in strategy
+        */
+        // TODO: How to handle Y = 0 when strategy is exited
+        uint amount = _shares.mul(
+            IStrategy(strategy).yieldTokenBalance()
+        ).div(totalSupply);
+
+        address yieldToken = IStrategy(strategy).yieldToken();
+        uint bal = IERC20(yieldToken).balanceOf(address(this));
         if (bal < amount) {
             // NOTE: can skip check for underflow here since bal < amount
-            IStrategy(strategy).withdraw(amount - bal);
-            uint balAfter = ERC20(token).balanceOf(address(this));
+            IStrategy(strategy).withdraw(amount - bal, _min);
+            uint balAfter = ERC20(yieldToken).balanceOf(address(this));
 
             if (balAfter < amount) {
                 amount = balAfter;
@@ -131,8 +132,6 @@ contract VaultV2 is IVault, ERC20 {
         require(amount >= _min); // dev: amount < min return
 
         _burn(msg.sender, amount);
-        IERC20(token).safeTransfer(msg.sender, amount);
-
-        emit Withdraw(msg.sender, amount);
+        IERC20(yieldToken).safeTransfer(msg.sender, amount);
     }
 }
--- contracts/interfaces/IVault.sol
+++ contracts/interfaces/IVault.sol
@@ -4,9 +4,8 @@ pragma solidity ^0.6.0;
 interface IVault {
     function token() external view returns (address);
     function strategy() external view returns (address);
-    function balance() external view returns (uint);
     function setStrategy(address _strategy) external;
-    function invest() external;
+    function invest(uint amount, uint min) external;
     function deposit(uint amount) external;
     function withdraw(uint amount, uint min) external;
 }
