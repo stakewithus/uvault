c5ba7500233042fbd9b5865b51cca53ea6a612e9
taz <tsk.nakamura@gmail.com>
2020-09-15 09:37:46 +0900

    rename Zap interface to DepositY

--- contracts/strategies/interfaces/DepositY.sol
+++ contracts/strategies/interfaces/DepositY.sol
@@ -1,7 +1,9 @@
 pragma solidity ^0.6.0;
 
+// https://github.com/curvefi/curve-contract/blob/master/contracts/pools/y/DepositY.vy
 // Contract address
 // 0xbBC81d23Ea2c3ec7e56D39296F0cbB648873a5d3
-interface Zap {
+
+interface DepositY {
   function calc_withdraw_one_coin(uint _token_amount, int128 i) external view returns (uint);
 }
