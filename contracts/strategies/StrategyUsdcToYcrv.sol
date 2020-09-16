9a6b76570709e4c430b98883b66cbc2870d81c5c
taz <tsk.nakamura@gmail.com>
2020-09-16 17:12:29 +0900

    StrategyUsdcTocCrv: set mainnet addresses

--- contracts/strategies/StrategyUsdcToYcrv.sol
+++ contracts/strategies/StrategyUsdcToYcrv.sol
@@ -21,7 +21,34 @@ contract StrategyUsdcToYcrv is IStrategy {
     address override public controller;
     address override public vault;
 
+    // TODO remove withdraw fee?
+    uint public withdrawFee = 50;
+    uint public withdrawFeeMax = 10000;
+
+    // performance fee sent to treasury when harvest() generates profit
+    uint public performanceFee = 50;
+    uint public performanceFeeMax = 10000;
+
     address constant private usdc = address(0xA0b86991c6218b36c1d19D4a2e9Eb0cE3606eB48);
+
+    // Curve
+    // cDAI/cUSDC
+    address constant private cCrv = address(0x845838DF265Dcd2c412A1Dc9e959c7d08537f8a2);
+    // DepositCompound
+    address constant private depositC = address(0xeB21209ae4C2c9FF2a86ACA31E123764A3B6Bc06);
+    // StableSwapCompound
+    address constant private curve = address(0xA2B47E3D5c44877cca798226B7B8118F9BFb7A56);
+    // cCrv Gauge
+    address constant private gauge = address(0x7ca5b0a2910B33e9759DC7dDB0413949071D7575);
+    // Minter
+    address constant private minter = address(0xd061D61a4d941c39E5453435B6345Dc261C2fcE0);
+    // DAO
+    address constant private crv = address(0xD533a949740bb3306d119CC777fa900bA034cd52);
+
+    // DEX related addresses
+    address constant private uniswap = address(0x7a250d5630B4cF539739dF2C5dAcb4c659F2488D);
+    address constant private weth = address(0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2); // used for crv <> weth <> dai route
+
     function underlyingToken() override external view returns (address) {
         return usdc;
     }
