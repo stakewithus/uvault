9b70ca3f37a59e03db626dff590a3d753f0eec9c
taz <tsk.nakamura@gmail.com>
2020-09-23 09:38:34 +0900

    gas reporter

--- truffle-config.js
+++ truffle-config.js
@@ -84,12 +84,16 @@ module.exports = {
   // Set default mocha options here, use special reporters etc.
   mocha: {
     // timeout: 100000
+    reporter: "eth-gas-reporter",
+    reporterOptions: {
+      currency: "USD",
+    },
   },
 
   // Configure your compilers
   compilers: {
     solc: {
-      // version: "0.5.1",    // Fetch exact version from solc-bin (default: truffle's version)
+      version: "0.5.17", // Fetch exact version from solc-bin (default: truffle's version)
       // docker: true,        // Use "0.5.1" you've installed locally with docker (default: false)
       // settings: {          // See the solidity docs for advice about optimization and evmVersion
       //  optimizer: {
