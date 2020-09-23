29d64d6d8067026681c74c5950fda73b16694282
taz <tsk.nakamura@gmail.com>
2020-09-23 15:29:53 +0900

    Vault: unit test invest

--- test/unit/test-vault.js
+++ test/unit/test-vault.js
@@ -15,7 +15,8 @@ contract("Vault", (accounts) => {
     erc20 = await ERC20Token.new();
   });
 
-  const MIN_WAIT_TIME = 100;
+  // set to 0 so we can immediately set strategy for other tests
+  const MIN_WAIT_TIME = 0;
 
   let vault;
   beforeEach(async () => {
@@ -45,7 +46,7 @@ contract("Vault", (accounts) => {
 
   //     it("should reject if token is zero address", async () => {
   //       await expect(Vault.new(ZERO_ADDRESS, "vault", "vault", MIN_WAIT_TIME)).to
-  //         .be.rejected;
+  //   .be.rejected;
   //     });
   //   });
 
@@ -219,84 +220,157 @@ contract("Vault", (accounts) => {
   //     });
   //   });
 
-  describe("withdraw", () => {
+  //   describe("withdraw", () => {
+  //     const sender = accounts[1];
+  //     const amount = new BN(10).pow(new BN(18));
+  //     const min = frac(amount, new BN(99), new BN(100));
+
+  //     beforeEach(async () => {
+  //       await erc20.mint(sender, amount);
+  //       await erc20.approve(vault.address, amount, { from: sender });
+  //       await vault.deposit(amount, { from: sender });
+  //     });
+
+  //     it("should withdraw from vault", async () => {
+  //       const snapshot = async () => {
+  //         return {
+  //           erc20: {
+  //             sender: await erc20.balanceOf(sender),
+  //             vault: await erc20.balanceOf(vault.address),
+  //           },
+  //           vault: {
+  //             balanceOf: {
+  //               sender: await vault.balanceOf(sender),
+  //             },
+  //             totalSupply: await vault.totalSupply(),
+  //           },
+  //         };
+  //       };
+
+  //       const before = await snapshot();
+  //       await vault.withdraw(amount, min, { from: sender });
+  //       const after = await snapshot();
+
+  //       // check erc20 balance
+  //       assert(
+  //         sub(after.erc20.sender, before.erc20.sender).gte(min),
+  //         "erc20 sender"
+  //       );
+  //       assert(
+  //         sub(before.erc20.vault, after.erc20.vault).gte(min),
+  //         "erc20 vault"
+  //       );
+
+  //       // check vault balance
+  //       assert(
+  //         eq(
+  //           after.vault.balanceOf.sender,
+  //           sub(before.vault.balanceOf.sender, amount)
+  //         ),
+  //         "vault sender"
+  //       );
+  //       assert(
+  //         eq(after.vault.totalSupply, sub(before.vault.totalSupply, amount)),
+  //         "total supply"
+  //       );
+  //     });
+
+  //     it("should reject if returned amount < min", async () => {
+  //       const min = add(amount, new BN(1));
+
+  //       await expect(
+  //         vault.withdraw(amount, min, { from: sender })
+  //       ).to.be.rejectedWith("withdraw amount < min");
+  //     });
+
+  //     it.skip("should withdraw from strategy", async () => {
+  //       // integration test
+  //       // cannot test without calling invest() and transferring tokens to strategy
+  //     });
+
+  //     it("should reject if balance = 0", async () => {
+  //       const bal = await vault.balanceOf(sender);
+  //       await vault.withdraw(bal, 0, { from: sender });
+
+  // await expect(vault.withdraw(bal, 0, { from: sender })).to.be.rejected;
+  //     });
+
+  //     it("should reject if amount = 0", async () => {
+  //       await expect(vault.withdraw(0, 0, { from: sender })).to.be.rejectedWith(
+  //         "shares = 0"
+  //       );
+  //     });
+  //   });
+
+  describe("invest", () => {
     const sender = accounts[1];
     const amount = new BN(10).pow(new BN(18));
-    const min = frac(amount, new BN(99), new BN(100));
 
+    let strategy;
     beforeEach(async () => {
       await erc20.mint(sender, amount);
       await erc20.approve(vault.address, amount, { from: sender });
       await vault.deposit(amount, { from: sender });
+
+      const controller = accounts[1];
+
+      strategy = await MockStrategy.new(
+        controller,
+        vault.address,
+        erc20.address,
+        { from: admin }
+      );
+
+      await vault.setNextStrategy(strategy.address, { from: admin });
+      await vault.switchStrategy({ from: admin });
     });
 
-    it("should withdraw from vault", async () => {
+    it("should invest", async () => {
       const snapshot = async () => {
         return {
-          erc20: {
-            sender: await erc20.balanceOf(sender),
-            vault: await erc20.balanceOf(vault.address),
-          },
           vault: {
-            balanceOf: {
-              sender: await vault.balanceOf(sender),
-            },
-            totalSupply: await vault.totalSupply(),
+            availableToInvest: await vault.availableToInvest(),
           },
         };
       };
 
       const before = await snapshot();
-      await vault.withdraw(amount, min, { from: sender });
+      await vault.invest({ from: admin });
       const after = await snapshot();
 
-      // check erc20 balance
       assert(
-        sub(after.erc20.sender, before.erc20.sender).gte(min),
-        "erc20 sender"
-      );
-      assert(
-        sub(before.erc20.vault, after.erc20.vault).gte(min),
-        "erc20 vault"
-      );
-
-      // check vault balance
-      assert(
-        eq(
-          after.vault.balanceOf.sender,
-          sub(before.vault.balanceOf.sender, amount)
-        ),
-        "vault sender"
-      );
-      assert(
-        eq(after.vault.totalSupply, sub(before.vault.totalSupply, amount)),
-        "total supply"
+        eq(await strategy._getDepositAmount_(), before.vault.availableToInvest),
+        "deposit"
       );
     });
 
-    it("should reject if returned amount < min", async () => {
-      const min = add(amount, new BN(1));
+    it("should not call strategy.deposit if balance = 0", async () => {
+      await vault.withdraw(amount, 0, { from: sender });
 
-      await expect(
-        vault.withdraw(amount, min, { from: sender })
-      ).to.be.rejectedWith("withdraw amount < min");
+      assert(eq(await vault.availableToInvest(), new BN(0)), "available");
+
+      await vault.invest({ from: admin });
+
+      assert(eq(await strategy._getDepositAmount_(), new BN(0)), "deposit");
     });
 
-    it.skip("should withdraw from strategy", async () => {
-      // integration test
-      // cannot test without calling invest() and transferring tokens to strategy
+    it("should reject if not admin", async () => {
+      await expect(vault.invest({ from: accounts[1] })).to.be.rejectedWith(
+        "!admin"
+      );
     });
 
-    it("should reject if balance = 0", async () => {
-      const bal = await vault.balanceOf(sender);
-      await vault.withdraw(bal, 0, { from: sender });
+    it("should reject if strategy not set", async () => {
+      const vault = await Vault.new(
+        erc20.address,
+        "vault",
+        "vault",
+        MIN_WAIT_TIME
+      );
+      assert.equal(await vault.strategy(), ZERO_ADDRESS, "strategy");
 
-      await expect(vault.withdraw(bal, 0, { from: sender })).to.be.rejected;
-    });
-
-    it("should reject if amount = 0", async () => {
-      await expect(vault.withdraw(0, 0, { from: sender })).to.be.rejectedWith(
-        "shares = 0"
+      await expect(vault.invest({ from: admin })).to.be.rejectedWith(
+        "strategy = zero address"
       );
     });
   });
