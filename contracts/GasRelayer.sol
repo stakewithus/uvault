54945bdfba1d8ddd4fd6fa32f8cce5e53d2fb6cb
taz <tsk.nakamura@gmail.com>
2020-09-24 13:14:25 +0900

    GasRelayer

--- contracts/GasRelayer.sol
+++ contracts/GasRelayer.sol
@@ -1,74 +1,74 @@
 pragma solidity 0.5.17;
 
-import "@openzeppelin/contracts/token/ERC20/SafeERC20.sol";
 import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 import "./interfaces/GasToken.sol";
 
-
 contract GasRelayer {
-    using SafeERC20 for IERC20;
-
-    address public owner;
+    address public admin;
     address public gasToken;
-    mapping(address => bool) public whitelist;
+    // mapping(address => bool) public whitelist;
 
     constructor(address _gasToken) public {
-        require(_gasToken != address(0)); // dev: gas token == zero address
+        require(_gasToken != address(0), "gas token = zero address");
 
-        owner = msg.sender;
+        admin = msg.sender;
         gasToken = _gasToken;
-        whitelist[msg.sender] = true;
+        // whitelist[msg.sender] = true;
     }
 
-    modifier onlyOwner() {
-        require(msg.sender == owner); // dev: !owner
+    modifier onlyAdmin() {
+        require(msg.sender == admin, "!admin");
         _;
     }
 
-    function setOwner(address _owner) external onlyOwner {
-        owner = _owner;
+    function setAdmin(address _admin) external onlyAdmin {
+        require(_admin != address(0), "admin = zero address");
+        admin = _admin;
     }
 
-    function setGasToken(address _gasToken) external onlyOwner {
+    function setGasToken(address _gasToken) external onlyAdmin {
+        require(_gasToken != address(0), "gas token = zero address");
         gasToken = _gasToken;
     }
 
-    function addWhitelist(address[5] calldata _addresses) external onlyOwner {
-        // NOTE: using uint8 for index
-        for (uint8 i = 0; i < 5; i++) {
-            address addr = _addresses[i];
-            if (addr != address(0)) {
-                whitelist[addr] = true;
-            }
-        }
-    }
+    // function addWhitelist(address[5] calldata _addresses) external onlyAdmin {
+    //     // NOTE: using uint8 for index
+    //     for (uint8 i = 0; i < 5; i++) {
+    //         address addr = _addresses[i];
+    //         if (addr != address(0)) {
+    //             whitelist[addr] = true;
+    //         }
+    //     }
+    // }
 
-    function removeWhitelist(address[5] calldata _addresses) external onlyOwner {
-        // NOTE: using uint8 for index
-        for (uint8 i = 0; i < 5; i++) {
-            address addr = _addresses[i];
-            if (addr != address(0)) {
-                whitelist[addr] = false;
-            }
-        }
-    }
+    // function removeWhitelist(address[5] calldata _addresses) external onlyAdmin {
+    //     // NOTE: using uint8 for index
+    //     for (uint8 i = 0; i < 5; i++) {
+    //         address addr = _addresses[i];
+    //         if (addr != address(0)) {
+    //             whitelist[addr] = false;
+    //         }
+    //     }
+    // }
 
     function mintGasToken(uint _value) external {
         GasToken(gasToken).mint(_value);
     }
 
-    function transferGasToken(address _to, uint _value) external onlyOwner {
-        IERC20(gasToken).safeTransfer(_to, _value);
+    function transferGasToken(address _to, uint _value) external onlyAdmin {
+        IERC20(gasToken).transfer(_to, _value);
     }
 
-    function relayTx(uint _value, address _to, bytes calldata _data) external {
-        require(whitelist[msg.sender]); // dev: !whitelist
+    function relayTx(uint _value, address _to, bytes calldata _data)
+        external onlyAdmin
+    {
+        // require(whitelist[msg.sender], "!whitelist");
 
         if (_value > 0) {
             GasToken(gasToken).freeUpTo(_value);
         }
 
         (bool success,) = _to.call(_data);
-        require(success); // dev: relay tx failed
+        require(success, "relay failed");
     }
 }
