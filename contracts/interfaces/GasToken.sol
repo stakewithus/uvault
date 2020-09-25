f2ee99454579a8f0fc2f49353b1a7c3dea28c2b2
taz <tsk.nakamura@gmail.com>
2020-09-25 10:12:09 +0900

    GasToken: add balanceOf

--- contracts/interfaces/GasToken.sol
+++ contracts/interfaces/GasToken.sol
@@ -4,5 +4,7 @@ interface GasToken {
     function mint(uint amount) external;
     function free(uint amount) external returns (bool);
     function freeUpTo(uint amount) external returns (uint);
+    // ERC20
     function transfer(address _to, uint _amount) external returns (bool);
+    function balanceOf(address account) external view returns (uint256);
 }
