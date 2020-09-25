092653b487345cd5f5c8a8e4521d5f2d267d7884
taz <tsk.nakamura@gmail.com>
2020-09-25 10:58:18 +0900

    integration test lib

--- test/mainnet/lib.js
+++ test/mainnet/lib.js
@@ -14,6 +14,91 @@ function encodeInvest(web3, vault) {
   );
 }
 
+function encodeRebalance(web3, vault) {
+  return web3.eth.abi.encodeFunctionCall(
+    {
+      name: "rebalance",
+      type: "function",
+      inputs: [
+        {
+          type: "address",
+          name: "vault",
+        },
+      ],
+    },
+    [vault]
+  );
+}
+
+function encodeSwitchStrategy(web3, vault) {
+  return web3.eth.abi.encodeFunctionCall(
+    {
+      name: "switchStrategy",
+      type: "function",
+      inputs: [
+        {
+          type: "address",
+          name: "vault",
+        },
+      ],
+    },
+    [vault]
+  );
+}
+
+function encodeHarvest(web3, strategy) {
+  return web3.eth.abi.encodeFunctionCall(
+    {
+      name: "harvest",
+      type: "function",
+      inputs: [
+        {
+          type: "address",
+          name: "strategy",
+        },
+      ],
+    },
+    [strategy]
+  );
+}
+
+function encodeWithdrawAll(web3, strategy) {
+  return web3.eth.abi.encodeFunctionCall(
+    {
+      name: "withdrawAll",
+      type: "function",
+      inputs: [
+        {
+          type: "address",
+          name: "strategy",
+        },
+      ],
+    },
+    [strategy]
+  );
+}
+
+function encodeExit(web3, strategy) {
+  return web3.eth.abi.encodeFunctionCall(
+    {
+      name: "exit",
+      type: "function",
+      inputs: [
+        {
+          type: "address",
+          name: "strategy",
+        },
+      ],
+    },
+    [strategy]
+  );
+}
+
 module.exports = {
   encodeInvest,
+  encodeRebalance,
+  encodeSwitchStrategy,
+  encodeHarvest,
+  encodeWithdrawAll,
+  encodeExit,
 };
