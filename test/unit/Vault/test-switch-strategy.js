5d3d50975a013fdbf61d1d68408763017d4f818a
taz <tsk.nakamura@gmail.com>
2020-09-28 13:37:57 +0900

    fix test

--- test/unit/Vault/test-switch-strategy.js
+++ test/unit/Vault/test-switch-strategy.js
@@ -1,12 +1,6 @@
 const BN = require("bn.js");
-const { expect } = require("../../setup");
-const {
-  ZERO_ADDRESS,
-  eq,
-  getBlockTimestamp,
-  timeout,
-  MAX_UINT,
-} = require("../../util");
+const {expect} = require("../../setup");
+const {ZERO_ADDRESS, eq, getBlockTimestamp, timeout, MAX_UINT} = require("../../util");
 const setup = require("./setup");
 
 const MockStrategy = artifacts.require("MockStrategy");
@@ -16,7 +10,7 @@ contract("Vault", (accounts) => {
   const MIN_WAIT_TIME = 2;
 
   const refs = setup(accounts, MIN_WAIT_TIME);
-  const { admin, controller } = refs;
+  const {admin, controller} = refs;
 
   let vault;
   let erc20;
@@ -29,22 +23,20 @@ contract("Vault", (accounts) => {
 
   describe("switchStrategy", () => {
     beforeEach(async () => {
-      await vault.setNextStrategy(strategy.address, { from: admin });
+      await vault.setNextStrategy(strategy.address, {from: admin});
     });
 
     it("should set new strategy", async () => {
-      const tx = await vault.switchStrategy({ from: controller });
+      const tx = await vault.switchStrategy({from: controller});
 
-      assert.equal(tx.logs[1].event, "SwitchStrategy", "event");
+      assert.equal(tx.logs[2].event, "SwitchStrategy", "event");
       assert.equal(
-        tx.logs[1].args.strategy,
+        tx.logs[2].args.strategy,
         strategy.address,
         "event arg switch strategy"
       );
       assert.equal(await vault.strategy(), strategy.address);
-      assert(
-        eq(await erc20.allowance(vault.address, strategy.address), MAX_UINT)
-      );
+      assert(eq(await erc20.allowance(vault.address, strategy.address), MAX_UINT));
       assert.isFalse(await strategy._exitWasCalled_(), "exit");
     });
 
@@ -54,21 +46,15 @@ contract("Vault", (accounts) => {
         controller,
         vault.address,
         erc20.address,
-        { from: admin }
+        {from: admin}
       );
 
       const snapshot = async () => {
         return {
           erc20: {
             allowance: {
-              oldStrategy: await erc20.allowance(
-                vault.address,
-                oldStrategy.address
-              ),
-              newStrategy: await erc20.allowance(
-                vault.address,
-                newStrategy.address
-              ),
+              oldStrategy: await erc20.allowance(vault.address, oldStrategy.address),
+              newStrategy: await erc20.allowance(vault.address, newStrategy.address),
             },
           },
           vault: {
@@ -77,20 +63,17 @@ contract("Vault", (accounts) => {
         };
       };
 
-      await vault.switchStrategy({ from: controller });
-      await vault.setNextStrategy(newStrategy.address, { from: admin });
+      await vault.switchStrategy({from: controller});
+      await vault.setNextStrategy(newStrategy.address, {from: admin});
       await timeout(MIN_WAIT_TIME);
 
       const before = await snapshot();
-      await vault.switchStrategy({ from: controller });
+      await vault.switchStrategy({from: controller});
       const after = await snapshot();
 
       assert.equal(before.vault.strategy, oldStrategy.address, "old strategy");
       assert.equal(after.vault.strategy, newStrategy.address, "new strategy");
-      assert(
-        eq(after.erc20.allowance.newStrategy, MAX_UINT),
-        "allowance new strategy"
-      );
+      assert(eq(after.erc20.allowance.newStrategy, MAX_UINT), "allowance new strategy");
       assert(
         eq(after.erc20.allowance.oldStrategy, new BN(0)),
         "allowance old strategy"
@@ -98,9 +81,9 @@ contract("Vault", (accounts) => {
     });
 
     it("should reject if not controller", async () => {
-      await expect(
-        vault.switchStrategy({ from: accounts[0] })
-      ).to.be.rejectedWith("!controller");
+      await expect(vault.switchStrategy({from: accounts[0]})).to.be.rejectedWith(
+        "!controller"
+      );
     });
 
     it("should reject if strategy is zero address", async () => {
@@ -112,27 +95,27 @@ contract("Vault", (accounts) => {
         MIN_WAIT_TIME
       );
 
-      await expect(
-        vault.switchStrategy({ from: controller })
-      ).to.be.rejectedWith("next strategy = zero address");
+      await expect(vault.switchStrategy({from: controller})).to.be.rejectedWith(
+        "next strategy = zero address"
+      );
     });
 
     it("should reject if same strategy", async () => {
-      vault.switchStrategy({ from: controller });
+      vault.switchStrategy({from: controller});
 
-      await expect(
-        vault.switchStrategy({ from: controller })
-      ).to.be.rejectedWith("next strategy = current strategy");
+      await expect(vault.switchStrategy({from: controller})).to.be.rejectedWith(
+        "next strategy = current strategy"
+      );
     });
 
     it("should reject timestamp < time lock", async () => {
-      await vault.switchStrategy({ from: controller });
+      await vault.switchStrategy({from: controller});
 
       const newStrategy = await MockStrategy.new(
         controller,
         vault.address,
         erc20.address,
-        { from: admin }
+        {from: admin}
       );
 
       const tx = await vault.setNextStrategy(newStrategy.address, {
@@ -141,14 +124,11 @@ contract("Vault", (accounts) => {
 
       const timestamp = await getBlockTimestamp(web3, tx);
 
-      assert(
-        (await vault.timeLock()).gte(timestamp + MIN_WAIT_TIME),
-        "time lock"
-      );
+      assert((await vault.timeLock()).gte(timestamp + MIN_WAIT_TIME), "time lock");
 
-      await expect(
-        vault.switchStrategy({ from: controller })
-      ).to.be.rejectedWith("timestamp < time lock");
+      await expect(vault.switchStrategy({from: controller})).to.be.rejectedWith(
+        "timestamp < time lock"
+      );
     });
   });
 });
