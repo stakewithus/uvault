f75af9767766374b422fc87f222de183ffb2e0be
taz <tsk.nakamura@gmail.com>
2020-09-29 23:24:44 +0900

    builder config

--- buidler.config.js
+++ buidler.config.js
@@ -25,6 +25,7 @@ module.exports = {
       accounts: [`0x${PRIVATE_KEY}`],
       gasPrice: "auto",
       // gasPrice: 5000000000, // 5 gwei
+      // gas: "auto",
     },
     coverage: {
       url: "http://127.0.0.1:8555", // Coverage launches its own ganache-cli client
