40035d7f43402c6e356a41a939b852f18ad58400
taz <tsk.nakamura@gmail.com>
2020-10-01 11:33:13 +0900

    Vault: test revoke strategy

---
+++ test/unit/Vault/test-revoke-strategy.js
@@ -0,0 +1,38 @@
+const {expect} = require("../../setup")
+const setup = require("./setup")
+const {assert} = require("chai")
+
+contract("Vault", (accounts) => {
+  const MIN_WAIT_TIME = 0
+
+  const refs = setup(accounts, MIN_WAIT_TIME)
+  const {admin, controller} = refs
+
+  let vault
+  let erc20
+  let strategy
+  beforeEach(() => {
+    vault = refs.vault
+    erc20 = refs.erc20
+    strategy = refs.strategy
+  })
+
+  describe("revokeStrategy", () => {
+    beforeEach(async () => {
+      await vault.setNextStrategy(strategy.address, {from: admin})
+      await vault.setStrategy(strategy.address, 0, {from: controller})
+    })
+
+    it("should revoke", async () => {
+      await vault.revokeStrategy(strategy.address, {from: admin})
+
+      assert.isFalse(await vault.strategies(strategy.address), "strategy")
+    })
+
+    it("should reject if not admin", async () => {
+      await expect(
+        vault.revokeStrategy(strategy.address, {from: accounts[1]})
+      ).to.be.rejectedWith("!admin")
+    })
+  })
+})
