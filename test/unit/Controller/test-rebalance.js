3e1abb3027b5b8d81b41f17b54b5db7260760c66
taz <tsk.nakamura@gmail.com>
2020-10-01 11:05:45 +0900

    Controller test rebalance

--- test/unit/Controller/test-rebalance.js
+++ test/unit/Controller/test-rebalance.js
@@ -1,4 +1,6 @@
+const BN = require("bn.js")
 const {expect} = require("../../setup")
+const {eq} = require("../../util")
 const setup = require("./setup")
 
 contract("Controller", (accounts) => {
@@ -12,27 +14,33 @@ contract("Controller", (accounts) => {
     vault = refs.vault
   })
 
+  const minIn = new BN(1)
+  const minOut = new BN(10)
+
   describe("rebalance", () => {
     it("should rebalance admin", async () => {
-      await controller.rebalance(vault.address, {from: admin})
+      await controller.rebalance(vault.address, minOut, minIn, {from: admin})
 
       assert(await vault._rebalanceWasCalled_(), "rebalance")
+      assert(eq(await vault._rebalanceMinOut_(), minOut), "min out")
+      assert(eq(await vault._rebalanceMinIn_(), minIn), "min in")
     })
 
     it("should rebalance gas relayer", async () => {
-      await controller.rebalance(vault.address, {from: gasRelayer})
+      await controller.rebalance(vault.address, minOut, minIn, {from: gasRelayer})
 
       assert(await vault._rebalanceWasCalled_(), "rebalance")
     })
 
     it("should reject if caller not authorized", async () => {
       await expect(
-        controller.rebalance(vault.address, {from: accounts[1]})
+        controller.rebalance(vault.address, minOut, minIn, {from: accounts[1]})
       ).to.be.rejectedWith("!authorized")
     })
 
     it("should reject invalid vault address", async () => {
-      await expect(controller.rebalance(accounts[1], {from: admin})).to.be.rejected
+      await expect(controller.rebalance(accounts[1], minOut, minIn, {from: admin})).to
+        .be.rejected
     })
   })
 })
