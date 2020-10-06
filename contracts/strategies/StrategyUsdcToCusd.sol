854824a07c34045091238ac97c1fd1cb607e29bc
taz <tsk.nakamura@gmail.com>
2020-10-06 15:26:03 +0900

    StrategyUsdcToCusd: replace constant addresses with private var

--- contracts/strategies/StrategyUsdcToCusd.sol
+++ contracts/strategies/StrategyUsdcToCusd.sol
@@ -28,27 +28,27 @@ contract StrategyUsdcToCusd is IStrategy {
     uint public performanceFee = 100;
     uint public constant PERFORMANCE_FEE_MAX = 10000;
 
-    address private constant USDC = address(0xA0b86991c6218b36c1d19D4a2e9Eb0cE3606eB48);
-    // address private constant DAI = address(0x6B175474E89094C44Da98b954EedeAC495271d0F);
+    address private usdc = address(0xA0b86991c6218b36c1d19D4a2e9Eb0cE3606eB48);
+    // address private  DAI = address(0x6B175474E89094C44Da98b954EedeAC495271d0F);
 
-    address private constant UNDERLYING = USDC;
+    address private underlying = usdc;
 
     // Curve
     // cDAI/cUSDC
-    address private constant CUSD = address(0x845838DF265Dcd2c412A1Dc9e959c7d08537f8a2);
+    address private cUsd = address(0x845838DF265Dcd2c412A1Dc9e959c7d08537f8a2);
     // DepositCompound
-    address private constant DEPOSIT_C = address(0xeB21209ae4C2c9FF2a86ACA31E123764A3B6Bc06);
+    address private depositC = address(0xeB21209ae4C2c9FF2a86ACA31E123764A3B6Bc06);
     // cUsd Gauge
-    address private constant GAUGE = address(0x7ca5b0a2910B33e9759DC7dDB0413949071D7575);
+    address private gauge = address(0x7ca5b0a2910B33e9759DC7dDB0413949071D7575);
     // Minter
-    address private constant MINTER = address(0xd061D61a4d941c39E5453435B6345Dc261C2fcE0);
+    address private minter = address(0xd061D61a4d941c39E5453435B6345Dc261C2fcE0);
     // DAO
-    address private constant CRV = address(0xD533a949740bb3306d119CC777fa900bA034cd52);
+    address private crv = address(0xD533a949740bb3306d119CC777fa900bA034cd52);
 
     // DEX related addresses
-    address private constant UNISWAP = address(0x7a250d5630B4cF539739dF2C5dAcb4c659F2488D);
+    address private uniswap = address(0x7a250d5630B4cF539739dF2C5dAcb4c659F2488D);
     // used for crv <> weth <> usdc route
-    address private constant WETH = address(0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2);
+    address private weth = address(0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2);
 
     constructor(address _controller, address _vault) public {
         require(_controller != address(0), "controller = zero address");
@@ -95,15 +95,15 @@ contract StrategyUsdcToCusd is IStrategy {
     }
 
     function underlyingToken() external view returns (address) {
-        return UNDERLYING;
+        return underlying;
     }
 
     function _underlyingBalance() internal view returns (uint) {
-        uint gaugeBal = Gauge(GAUGE).balanceOf(address(this));
+        uint gaugeBal = Gauge(gauge).balanceOf(address(this));
 
         // DAI  = 0
-        // USDC = 1
-        return DepositCompound(DEPOSIT_C).calc_withdraw_one_coin(gaugeBal, int128(1));
+        // usdc = 1
+        return DepositCompound(depositC).calc_withdraw_one_coin(gaugeBal, int128(1));
     }
 
     /*
@@ -118,20 +118,20 @@ contract StrategyUsdcToCusd is IStrategy {
     */
     function _depositUnderlying() internal {
         // underlying to cUsd
-        uint underlyingBal = IERC20(UNDERLYING).balanceOf(address(this));
+        uint underlyingBal = IERC20(underlying).balanceOf(address(this));
         if (underlyingBal > 0) {
-            IERC20(UNDERLYING).safeApprove(DEPOSIT_C, 0);
-            IERC20(UNDERLYING).safeApprove(DEPOSIT_C, underlyingBal);
+            IERC20(underlying).safeApprove(depositC, 0);
+            IERC20(underlying).safeApprove(depositC, underlyingBal);
             // mint cUsd
-            DepositCompound(DEPOSIT_C).add_liquidity([0, underlyingBal], 0);
+            DepositCompound(depositC).add_liquidity([0, underlyingBal], 0);
         }
 
         // stake cUsd into Gauge
-        uint cUsdBal = IERC20(CUSD).balanceOf(address(this));
+        uint cUsdBal = IERC20(cUsd).balanceOf(address(this));
         if (cUsdBal > 0) {
-            IERC20(CUSD).safeApprove(GAUGE, 0);
-            IERC20(CUSD).safeApprove(GAUGE, cUsdBal);
-            Gauge(GAUGE).deposit(cUsdBal);
+            IERC20(cUsd).safeApprove(gauge, 0);
+            IERC20(cUsd).safeApprove(gauge, cUsdBal);
+            Gauge(gauge).deposit(cUsdBal);
         }
     }
 
@@ -142,20 +142,20 @@ contract StrategyUsdcToCusd is IStrategy {
     function deposit(uint _underlyingAmount) external onlyVault {
         require(_underlyingAmount > 0, "underlying = 0");
 
-        IERC20(UNDERLYING).safeTransferFrom(vault, address(this), _underlyingAmount);
+        IERC20(underlying).safeTransferFrom(vault, address(this), _underlyingAmount);
         _depositUnderlying();
     }
 
     function _withdrawUnderlying(uint _cUsdAmount) internal {
         // withdraw cUsd from  Gauge
-        Gauge(GAUGE).withdraw(_cUsdAmount);
+        Gauge(gauge).withdraw(_cUsdAmount);
 
         // withdraw dai and usdc
-        uint cUsdBal = IERC20(CUSD).balanceOf(address(this));
-        IERC20(CUSD).safeApprove(DEPOSIT_C, 0);
-        IERC20(CUSD).safeApprove(DEPOSIT_C, cUsdBal);
+        uint cUsdBal = IERC20(cUsd).balanceOf(address(this));
+        IERC20(cUsd).safeApprove(depositC, 0);
+        IERC20(cUsd).safeApprove(depositC, cUsdBal);
         // NOTE: creates cUsd dust so we donate it
-        DepositCompound(DEPOSIT_C).remove_liquidity_one_coin(cUsdBal, int128(1), 0, true);
+        DepositCompound(depositC).remove_liquidity_one_coin(cUsdBal, int128(1), 0, true);
 
         // Now we have usdc
     }
@@ -179,7 +179,7 @@ contract StrategyUsdcToCusd is IStrategy {
         u / U = c / C
         c = u / U * C
         */
-        uint gaugeBal = Gauge(GAUGE).balanceOf(address(this));
+        uint gaugeBal = Gauge(gauge).balanceOf(address(this));
         uint cUsdAmount = _underlyingAmount.mul(gaugeBal).div(totalUnderlying);
 
         if (cUsdAmount > 0) {
@@ -187,22 +187,22 @@ contract StrategyUsdcToCusd is IStrategy {
         }
 
         // transfer underlying token to vault
-        uint underlyingBal = IERC20(UNDERLYING).balanceOf(address(this));
+        uint underlyingBal = IERC20(underlying).balanceOf(address(this));
         if (underlyingBal > 0) {
-            IERC20(UNDERLYING).safeTransfer(vault, underlyingBal);
+            IERC20(underlying).safeTransfer(vault, underlyingBal);
         }
     }
 
     function _withdrawAll() internal {
         // gauge balance is same unit as cUsd
-        uint gaugeBal = Gauge(GAUGE).balanceOf(address(this));
+        uint gaugeBal = Gauge(gauge).balanceOf(address(this));
         if (gaugeBal > 0) {
             _withdrawUnderlying(gaugeBal);
         }
 
-        uint underlyingBal = IERC20(UNDERLYING).balanceOf(address(this));
+        uint underlyingBal = IERC20(underlying).balanceOf(address(this));
         if (underlyingBal > 0) {
-            IERC20(UNDERLYING).safeTransfer(vault, underlyingBal);
+            IERC20(underlying).safeTransfer(vault, underlyingBal);
         }
     }
 
@@ -218,21 +218,21 @@ contract StrategyUsdcToCusd is IStrategy {
     @notice Claim CRV and swap for underlying token
     */
     function _crvToUnderlying() internal {
-        Minter(MINTER).mint(GAUGE);
+        Minter(minter).mint(gauge);
 
-        uint crvBal = IERC20(CRV).balanceOf(address(this));
+        uint crvBal = IERC20(crv).balanceOf(address(this));
         if (crvBal > 0) {
             // use Uniswap to exchange CRV for underlying
-            IERC20(CRV).safeApprove(UNISWAP, 0);
-            IERC20(CRV).safeApprove(UNISWAP, crvBal);
+            IERC20(crv).safeApprove(uniswap, 0);
+            IERC20(crv).safeApprove(uniswap, crvBal);
 
             // route crv > weth > underlying
             address[] memory path = new address[](3);
-            path[0] = CRV;
-            path[1] = WETH;
-            path[2] = UNDERLYING;
+            path[0] = crv;
+            path[1] = weth;
+            path[2] = underlying;
 
-            Uniswap(UNISWAP).swapExactTokensForTokens(crvBal, uint(0), path, address(this), now.add(1800));
+            Uniswap(uniswap).swapExactTokensForTokens(crvBal, uint(0), path, address(this), now.add(1800));
             // NOTE: Now this contract has underlying token
         }
     }
@@ -244,7 +244,7 @@ contract StrategyUsdcToCusd is IStrategy {
     function harvest() external onlyController {
         _crvToUnderlying();
 
-        uint usdcBal = IERC20(USDC).balanceOf(address(this));
+        uint usdcBal = IERC20(usdc).balanceOf(address(this));
         if (usdcBal > 0) {
             // transfer fee to treasury
             uint fee = usdcBal.mul(performanceFee).div(PERFORMANCE_FEE_MAX);
@@ -252,7 +252,7 @@ contract StrategyUsdcToCusd is IStrategy {
                 address treasury = IController(controller).treasury();
                 require(treasury != address(0), "treasury = zero address");
 
-                IERC20(USDC).safeTransfer(treasury, fee);
+                IERC20(usdc).safeTransfer(treasury, fee);
             }
 
             // deposit remaining underlying for cUsd
@@ -271,8 +271,8 @@ contract StrategyUsdcToCusd is IStrategy {
     }
 
     function sweep(address _token) external onlyAdmin {
-        require(_token != UNDERLYING, "token = underlying");
-        require(_token != CUSD, "token = cUsd");
+        require(_token != underlying, "token = underlying");
+        require(_token != cUsd, "token = cUsd");
         IERC20(_token).transfer(admin, IERC20(_token).balanceOf(address(this)));
     }
 }
