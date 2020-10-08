a4f95cdb2455ed3cbea63b2e026b100a7fa0d285
taz <tsk.nakamura@gmail.com>
2020-10-08 16:17:56 +0900

    fix mainnet test

--- test/mainnet/StrategyUsdcToCusd/setup.js
+++ test/mainnet/StrategyUsdcToCusd/setup.js
@@ -4,7 +4,7 @@ const {sendEther} = require("../../util")
 const IERC20 = artifacts.require("IERC20")
 const Gauge = artifacts.require("Gauge")
 const Controller = artifacts.require("Controller")
-const StrategyUsdcToCusd = artifacts.require("StrategyUsdcToCusd")
+const StrategyUsdcToCusdMainnet = artifacts.require("StrategyUsdcToCusdMainnet")
 
 module.exports = (accounts) => {
   const admin = accounts[0]
@@ -36,7 +36,7 @@ module.exports = (accounts) => {
     refs.cGauge = await Gauge.at(CGAUGE)
     refs.crv = await IERC20.at(CRV)
     refs.controller = await Controller.new(treasury, gasRelayer)
-    refs.strategy = await StrategyUsdcToCusd.new(refs.controller.address, vault)
+    refs.strategy = await StrategyUsdcToCusdMainnet.new(refs.controller.address, vault)
   })
 
   return refs
