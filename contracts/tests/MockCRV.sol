8531446a07b50b7dfb2d7e6779bd5b96c3e3ac9f
taz <tsk.nakamura@gmail.com>
2020-10-08 23:12:47 +0900

    remove mocks

--- contracts/tests/MockCRV.sol
+++
@@ -1,8 +0,0 @@
-pragma solidity 0.5.17;
-
-import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
-import "@openzeppelin/contracts/token/ERC20/ERC20Detailed.sol";
-
-contract MockCRV is ERC20, ERC20Detailed {
-    constructor() public ERC20Detailed("CRV", "CRV", 18) {}
-}
--- contracts/tests/MockWETH.sol
+++
@@ -1,8 +0,0 @@
-pragma solidity 0.5.17;
-
-import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
-import "@openzeppelin/contracts/token/ERC20/ERC20Detailed.sol";
-
-contract MockWETH is ERC20, ERC20Detailed {
-    constructor() public ERC20Detailed("WETH", "WETH", 18) {}
-}
