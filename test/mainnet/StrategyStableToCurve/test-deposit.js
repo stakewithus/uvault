bcdd6dcc3cb33a7d017989c4eb36bedb66bb3b01
taz <tsk.nakamura@gmail.com>
2020-10-13 13:55:26 +0900

    rename variables

--- test/mainnet/StrategyStableToCurve/test-deposit.js
+++ test/mainnet/StrategyStableToCurve/test-deposit.js
@@ -23,13 +23,13 @@ module.exports = (contractName, setup, { DECIMALS, UNDERLYING_TO_CURVE_DECIMALS 
     })
 
     it("should deposit", async () => {
-      const amount = new BN(10).pow(DECIMALS)
+      const depositAmount = new BN(10).pow(DECIMALS)
 
       // transfer underlying to vault
-      await underlying.transfer(vault, amount, {from: whale})
+      await underlying.transfer(vault, depositAmount, {from: whale})
 
       // approve strategy to spend underlying from vault
-      await underlying.approve(strategy.address, amount, {from: vault})
+      await underlying.approve(strategy.address, depositAmount, {from: vault})
 
       const snapshot = getSnapshot({
         underlying,
@@ -42,19 +42,19 @@ module.exports = (contractName, setup, { DECIMALS, UNDERLYING_TO_CURVE_DECIMALS 
       })
 
       const before = await snapshot()
-      await strategy.deposit(amount, {from: vault})
+      await strategy.deposit(depositAmount, {from: vault})
       const after = await snapshot()
 
       // minimum amount of underlying that can be withdrawn
-      const minUnderlying = frac(amount, new BN(99), new BN(100))
+      const minUnderlying = frac(depositAmount, new BN(99), new BN(100))
       // minimum amount of cUnderlying minted
-      const minC = frac(amount.mul(UNDERLYING_TO_CURVE_DECIMALS), new BN(95), new BN(100))
+      const minC = frac(depositAmount.mul(UNDERLYING_TO_CURVE_DECIMALS), new BN(95), new BN(100))
 
       const gaugeDiff = sub(after.gauge.strategy, before.gauge.strategy)
       const underlyingDiff = sub(after.strategy.totalAssets, before.strategy.totalAssets)
 
       // underlying transferred from vault to strategy
-      assert(eq(after.underlying.vault, sub(before.underlying.vault, amount)), "underlying vault")
+      assert(eq(after.underlying.vault, sub(before.underlying.vault, depositAmount)), "underlying vault")
       assert(underlyingDiff.gte(minUnderlying), "min underlying")
       assert(gaugeDiff.gte(minC), "min gauge")
     })
