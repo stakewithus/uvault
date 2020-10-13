89ec639a6fc253577377ecdafec1859920609b2b
taz <tsk.nakamura@gmail.com>
2020-10-13 13:59:33 +0900

    StrategyStableToCurve: test withdraw

---
+++ test/mainnet/StrategyStableToCurve/test-withdraw.js
@@ -0,0 +1,69 @@
+const BN = require("bn.js")
+const {eq, sub, frac } = require("../../util")
+const {getSnapshot} = require("./lib")
+
+module.exports = (contractName, setup, { DECIMALS }) => {
+  contract(contractName, (accounts) => {
+    const depositAmount = new BN(10).pow(DECIMALS)
+
+    const refs = setup(accounts)
+    const {vault, treasury, whale} = refs
+
+    let underlying
+    let cUnderlying
+    let gauge
+    let crv
+    let controller
+    let strategy
+    beforeEach(async () => {
+      underlying = refs.underlying
+      cUnderlying = refs.cUnderlying
+      gauge = refs.gauge
+      crv = refs.crv
+      controller = refs.controller
+      strategy = refs.strategy
+
+      // deposit underlying into vault
+      await underlying.transfer(vault, depositAmount, {from: whale})
+
+      // deposit underlying into strategy
+      await underlying.approve(strategy.address, depositAmount, {from: vault})
+      await strategy.deposit(depositAmount, {from: vault})
+    })
+
+    it("should withdraw", async () => {
+      const snapshot = getSnapshot({
+        underlying,
+        cUnderlying,
+        crv,
+        gauge,
+        strategy,
+        treasury,
+        vault,
+      })
+
+      // withdraw amount may be < deposit amount
+      // so here we get the maximum redeemable amount
+      const withdrawAmount = await strategy.totalAssets()
+
+      const before = await snapshot()
+      await strategy.withdraw(withdrawAmount, {from: vault})
+      const after = await snapshot()
+
+      // minimum amount of underlying that can be withdrawn
+      const minUnderlying = frac(depositAmount, new BN(99), new BN(100))
+
+      // check balance of underlying transferred to treasury and vault
+      const fee = sub(after.underlying.treasury, before.underlying.treasury)
+      const underlyingDiff = sub(after.underlying.vault, before.underlying.vault)
+
+      assert(fee.gte(new BN(0)), "fee")
+      assert(underlyingDiff.gte(minUnderlying), "underlying diff")
+
+      // check strategy does not have any underlying
+      assert(eq(after.underlying.strategy, new BN(0)), "underlying strategy")
+      // check strategy does not have any cUnderlying dust
+      assert(eq(after.cUnderlying.strategy, new BN(0)), "cUnderlying strategy")
+    })
+  })
+}
--- test/mainnet/StrategyUsdcToCusdMainnet/test-withdraw.js
+++ test/mainnet/StrategyUsdcToCusdMainnet/test-withdraw.js
@@ -1,69 +1,7 @@
-const BN = require("bn.js")
-const {USDC_WHALE} = require("../../config")
-const {eq, sub, frac, USDC_DECIMALS} = require("../../util")
-const {getSnapshot} = require("./lib")
+const {USDC_DECIMALS} = require("../../util")
 const setup = require("./setup")
+const test = require("../StrategyStableToCurve/test-withdraw")
 
-contract("StrategyUsdcToCusdMainnet", (accounts) => {
-  const depositAmount = new BN(10).pow(USDC_DECIMALS)
-
-  const refs = setup(accounts)
-  const {vault, treasury} = refs
-
-  let usdc
-  let cUsd
-  let cGauge
-  let crv
-  let controller
-  let strategy
-  beforeEach(async () => {
-    usdc = refs.usdc
-    cUsd = refs.cUsd
-    cGauge = refs.cGauge
-    crv = refs.crv
-    controller = refs.controller
-    strategy = refs.strategy
-
-    // deposit USDC into vault
-    await usdc.transfer(vault, depositAmount, {from: USDC_WHALE})
-
-    // deposit USDC into strategy
-    await usdc.approve(strategy.address, depositAmount, {from: vault})
-    await strategy.deposit(depositAmount, {from: vault})
-  })
-
-  it("should withdraw", async () => {
-    const snapshot = getSnapshot({
-      usdc,
-      cUsd,
-      crv,
-      cGauge,
-      strategy,
-      treasury,
-      vault,
-    })
-
-    // withdraw amount may be < deposit amount
-    // so here we get the maximum redeemable amount
-    const withdrawAmount = await strategy.totalAssets()
-
-    const before = await snapshot()
-    await strategy.withdraw(withdrawAmount, {from: vault})
-    const after = await snapshot()
-
-    // minimum amount of USDC that can be withdrawn
-    const minUsdc = frac(depositAmount, new BN(99), new BN(100))
-
-    // check balance of usdc transferred to treasury and vault
-    const fee = sub(after.usdc.treasury, before.usdc.treasury)
-    const usdcDiff = sub(after.usdc.vault, before.usdc.vault)
-
-    assert(fee.gte(new BN(0)), "fee")
-    assert(usdcDiff.gte(minUsdc), "usdc diff")
-
-    // check strategy does not have any USDC
-    assert(eq(after.usdc.strategy, new BN(0)), "usdc strategy")
-    // check strategy does not have any cUSD dust
-    assert(eq(after.cUsd.strategy, new BN(0)), "cUsd strategy")
-  })
+test("StrategyUsdcToCusdMainnet", setup, {
+  DECIMALS: USDC_DECIMALS,
 })
