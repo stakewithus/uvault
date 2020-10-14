d0ac64f3bed6c62620cf3e02233e6dd15fb80d75
taz <tsk.nakamura@gmail.com>
2020-10-14 16:16:45 +0900

    mainnet testnet secrets

--- buidler.config.ts
+++ buidler.config.ts
@@ -3,7 +3,8 @@ import {BuidlerConfig, usePlugin} from "@nomiclabs/buidler/config"
 
 usePlugin("@nomiclabs/buidler-ethers")
 
-const PRIVATE_KEY = fs.readFileSync(".secret").toString().trim()
+const MAINNET_PRIVATE_KEY = fs.readFileSync(".secret.mainnet").toString().trim()
+const TESTNET_PRIVATE_KEY = fs.readFileSync(".secret.testnet").toString().trim()
 
 const INFURA_API_KEY = process.env.INFURA_API_KEY
 // const ETHERSCAN_API_KEY = process.env.ETHERSCAN_API_KEY
@@ -17,14 +18,14 @@ const config: BuidlerConfig = {
   networks: {
     ropsten: {
       url: `https://ropsten.infura.io/v3/${INFURA_API_KEY}`,
-      accounts: [`0x${PRIVATE_KEY}`],
+      accounts: [`0x${TESTNET_PRIVATE_KEY}`],
       gasPrice: "auto",
       // gasPrice: 5000000000, // 5 gwei
       // gas: "auto",
     },
     mainnet: {
       url: `https://mainnet.infura.io/v3/${INFURA_API_KEY}`,
-      accounts: [`0x${PRIVATE_KEY}`],
+      accounts: [`0x${MAINNET_PRIVATE_KEY}`],
       gasPrice: "auto",
       // gasPrice: 5000000000, // 5 gwei
       // gas: "auto",
