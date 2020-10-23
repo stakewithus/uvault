c892184b8ae6825a80e04bcc9223487f7684c239
taz <tsk.nakamura@gmail.com>
2020-10-23 14:00:19 +0900

    Start Pickle strategy

---
+++ contracts/interfaces/pickle/PickleJar.sol
@@ -0,0 +1,15 @@
+pragma solidity 0.5.17;
+
+interface PickleJar {
+    function balanceOf(address _shares) external view returns (uint);
+
+    /*
+    @notice returns price of token / share
+    @dev ratio is multiplied by 10 ** 18
+    */
+    function getRatio() external view returns (uint);
+
+    function deposit(uint _amount) external;
+
+    function withdraw(uint _amount) external;
+}
