136f39d75645225dc1a28cf54a88f49919611ce2
taz <tsk.nakamura@gmail.com>
2020-11-26 16:42:40 +0900

    StrategyBusd

---
+++ contracts/interfaces/curve/DepositBusd.sol
@@ -0,0 +1,24 @@
+// SPDX-License-Identifier: AGPL-3.0-or-later
+pragma solidity 0.6.11;
+
+interface DepositBusd {
+    /*
+    0 DAI
+    1 USDC
+    2 USDT
+    3 BUSD
+    */
+    function add_liquidity(uint[4] memory amounts, uint min) external;
+
+    function remove_liquidity_one_coin(
+        uint amount,
+        int128 index,
+        uint min,
+        bool donateDust
+    ) external;
+
+    function calc_withdraw_one_coin(uint amount, int128 index)
+        external
+        view
+        returns (uint);
+}
