a2c4fbd97cfd715380d71feb7c7d6a7b99a2a94b
t4sk <tsk.nakamura@gmail.com>
2021-01-18 15:42:48 +0900

    fix deploy controller v2 script

--- scripts/deploy-controller.ts
+++ scripts/deploy-controller.ts
@@ -7,16 +7,16 @@ import { deploy, getAddress } from "./lib"
 usage:
 env $(cat .env) npx hardhat deploy:controller --network ropsten --dev false
 */
-task("deploy:controller", "Deploy controller")
+task("deploy:controller", "Deploy controller (v2)")
   .addOptionalParam("dev", "Use mainnet dev", "false")
   .setAction(async (args, hre) => {
     assert(args.dev === "false" || args.dev === "true", `invalid arg dev: ${args.dev}`)
     const dev = args.dev === "true"
 
-    await deploy(hre, "Controller", dev, async (_account, network) => {
+    await deploy(hre, "ControllerV2", dev, async (_account, network) => {
       const treasury = getAddress(config, network, dev, "treasury")
 
-      const Controller = await hre.ethers.getContractFactory("Controller")
-      return Controller.deploy(treasury)
+      const ControllerV2 = await hre.ethers.getContractFactory("ControllerV2")
+      return ControllerV2.deploy(treasury)
     })
   })
