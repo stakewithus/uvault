7427f30fd793e056edbc5b43b57715fc60ba4508
t4sk <tsk.nakamura@gmail.com>
2021-01-19 14:38:48 +0900

    StrategyGusdV2

---
+++ contracts/strategies/StrategyGusdUsdcV2.sol
@@ -0,0 +1,14 @@
+// SPDX-License-Identifier: AGPL-3.0-or-later
+pragma solidity 0.6.11;
+
+import "./StrategyGusdV2.sol";
+
+contract StrategyGusdUsdcV2 is StrategyGusdV2 {
+    constructor(address _controller, address _vault)
+        public
+        StrategyGusdV2(_controller, _vault, USDC)
+    {
+        // usdc
+        underlyingIndex = 2;
+    }
+}
---
+++ contracts/strategies/StrategyGusdV2.sol
@@ -0,0 +1,212 @@
+// SPDX-License-Identifier: AGPL-3.0-or-later
+pragma solidity 0.6.11;
+
+import "../interfaces/curve/StableSwapGusd.sol";
+import "../interfaces/curve/StableSwap3Pool.sol";
+import "../interfaces/curve/DepositGusd.sol";
+import "../interfaces/curve/LiquidityGauge.sol";
+import "../interfaces/curve/Minter.sol";
+import "../StrategyBaseV2.sol";
+import "../UseUniswap.sol";
+
+contract StrategyGusdV2 is StrategyBaseV2, UseUniswap {
+    // 3Pool StableSwap
+    address private constant BASE_POOL = 0xbEbc44782C7dB0a1A60Cb6fe97d0b483032FF1C7;
+    // GUSD StableSwap
+    address private constant SWAP = 0x4f062658EaAF2C1ccf8C8e36D6824CDf41167956;
+    address private constant GUSD = 0x056Fd409E1d7A124BD7017459dFEa2F387b6d5Cd;
+    address internal constant DAI = 0x6B175474E89094C44Da98b954EedeAC495271d0F;
+    address internal constant USDC = 0xA0b86991c6218b36c1d19D4a2e9Eb0cE3606eB48;
+    address internal constant USDT = 0xdAC17F958D2ee523a2206206994597C13D831ec7;
+
+    // GUSD = 0 | DAI = 1 | USDC = 2 | USDT = 3
+    uint internal underlyingIndex;
+    // precision to convert 10 ** 18  to underlying decimals
+    uint[4] private PRECISION_DIVS = [1e16, 1, 1e12, 1e12];
+
+    // Curve //
+    // liquidity provider token (GUSD / 3CRV)
+    address private constant LP = 0xD2967f45c4f384DEEa880F807Be904762a3DeA07;
+    // DepositGusd
+    address private constant POOL = 0x64448B78561690B70E17CBE8029a3e5c1bB7136e;
+    // LiquidityGauge
+    address private constant GAUGE = 0xC5cfaDA84E902aD92DD40194f0883ad49639b023;
+    // Minter
+    address private constant MINTER = 0xd061D61a4d941c39E5453435B6345Dc261C2fcE0;
+    // DAO
+    address private constant CRV = 0xD533a949740bb3306d119CC777fa900bA034cd52;
+
+    // prevent slippage from deposit / withdraw into Curve ppol
+    uint public slippage = 100;
+    uint private constant SLIPPAGE_MAX = 10000;
+
+    constructor(
+        address _controller,
+        address _vault,
+        address _underlying
+    ) public StrategyBaseV2(_controller, _vault, _underlying) {}
+
+    function setSlippage(uint _slippage) external onlyAdmin {
+        require(_slippage <= SLIPPAGE_MAX, "slippage > max");
+        slippage = _slippage;
+    }
+
+    function _totalAssets() internal view override returns (uint) {
+        uint lpBal = LiquidityGauge(GAUGE).balanceOf(address(this));
+        uint pricePerShare = StableSwapGusd(SWAP).get_virtual_price();
+
+        return lpBal.mul(pricePerShare).div(PRECISION_DIVS[underlyingIndex]) / 1e18;
+    }
+
+    /*
+    @notice deposit token into curve
+    */
+    function _deposit(address _token, uint _index) internal {
+        // token to LP
+        uint bal = IERC20(_token).balanceOf(address(this));
+        if (bal > 0) {
+            IERC20(_token).safeApprove(POOL, 0);
+            IERC20(_token).safeApprove(POOL, bal);
+
+            // mint LP
+            uint[4] memory amounts;
+            amounts[_index] = bal;
+
+            /*
+            shares = underlying amount * precision div * 1e18 / price per share
+            */
+            uint pricePerShare = StableSwapGusd(POOL).get_virtual_price();
+            uint shares = bal.mul(PRECISION_DIVS[_index]).mul(1e18).div(pricePerShare);
+            uint min = shares.mul(SLIPPAGE_MAX - slippage).div(SLIPPAGE_MAX);
+
+            DepositGusd(POOL).add_liquidity(amounts, min);
+        }
+
+        // stake into LiquidityGauge
+        uint lpBal = IERC20(LP).balanceOf(address(this));
+        if (lpBal > 0) {
+            IERC20(LP).safeApprove(GAUGE, 0);
+            IERC20(LP).safeApprove(GAUGE, lpBal);
+            LiquidityGauge(GAUGE).deposit(lpBal);
+        }
+    }
+
+    /*
+    @notice Deposits underlying to LiquidityGauge
+    */
+    function _depositUnderlying() internal override {
+        _deposit(underlying, underlyingIndex);
+    }
+
+    function _getTotalShares() internal view override returns (uint) {
+        return LiquidityGauge(GAUGE).balanceOf(address(this));
+    }
+
+    function _withdrawUnderlying(uint _lpAmount) internal override {
+        // withdraw LP from  LiquidityGauge
+        LiquidityGauge(GAUGE).withdraw(_lpAmount);
+
+        // withdraw underlying //
+        uint lpBal = IERC20(LP).balanceOf(address(this));
+
+        // remove liquidity
+        IERC20(LP).safeApprove(POOL, 0);
+        IERC20(LP).safeApprove(POOL, lpBal);
+
+        /*
+        underlying amount = (shares * price per shares) / (1e18 * precision div)
+        */
+        uint pricePerShare = StableSwapGusd(POOL).get_virtual_price();
+        uint underlyingAmount =
+            lpBal.mul(pricePerShare).div(PRECISION_DIVS[underlyingIndex]) / 1e18;
+        uint min = underlyingAmount.mul(SLIPPAGE_MAX - slippage).div(SLIPPAGE_MAX);
+        // withdraw creates LP dust
+        DepositGusd(POOL).remove_liquidity_one_coin(
+            lpBal,
+            int128(underlyingIndex),
+            min
+        );
+        // Now we have underlying
+    }
+
+    /*
+    @notice Returns address and index of token with lowest balance in Curve POOL
+    */
+
+    function _getMostPremiumToken() internal view returns (address, uint) {
+        /*
+        Swapping small amount of CRV (< $0.01) with GUSD can cause Uniswap to fail
+        since 0 GUSD is returned from the trade.
+        So we skip buying GUSD
+        */
+        uint[3] memory balances;
+        balances[0] = StableSwap3Pool(BASE_POOL).balances(0); // DAI
+        balances[1] = StableSwap3Pool(BASE_POOL).balances(1).mul(1e12); // USDC
+        balances[2] = StableSwap3Pool(BASE_POOL).balances(2).mul(1e12); // USDT
+
+        uint minIndex = 0;
+        for (uint i = 1; i < balances.length; i++) {
+            if (balances[i] <= balances[minIndex]) {
+                minIndex = i;
+            }
+        }
+
+        /*
+        DAI  1
+        USDC 2
+        USDT 3
+        */
+
+        if (minIndex == 0) {
+            return (DAI, 1);
+        }
+        if (minIndex == 1) {
+            return (USDC, 2);
+        }
+        return (USDT, 3);
+    }
+
+    function _swapCrvFor(address _token) private {
+        Minter(MINTER).mint(GAUGE);
+
+        uint crvBal = IERC20(CRV).balanceOf(address(this));
+        if (crvBal > 0) {
+            _swap(CRV, _token, crvBal);
+            // Now this contract has token
+        }
+    }
+
+    /*
+    @notice Claim CRV and deposit most premium token into Curve
+    */
+    function harvest() external override onlyAuthorized {
+        (address token, uint index) = _getMostPremiumToken();
+
+        _swapCrvFor(token);
+
+        uint bal = IERC20(token).balanceOf(address(this));
+        if (bal > 0) {
+            // transfer fee to treasury
+            uint fee = bal.mul(performanceFee) / PERFORMANCE_FEE_MAX;
+            if (fee > 0) {
+                address treasury = IControllerV2(controller).treasury();
+                require(treasury != address(0), "treasury = zero address");
+
+                IERC20(token).safeTransfer(treasury, fee);
+            }
+
+            _deposit(token, index);
+        }
+    }
+
+    /*
+    @notice Exit strategy by harvesting CRV to underlying token and then
+            withdrawing all underlying to vault
+    @dev Must return all underlying token to vault
+    @dev Caller should implement guard agains slippage
+    */
+    function exit() external override onlyAuthorized {
+        _swapCrvFor(underlying);
+        _withdrawAll();
+    }
+}
