619b2090f18777e11a6e8885605ef1a16c2739a8
t4sk <tsk.nakamura@gmail.com>
2021-01-20 11:26:23 +0900

    github actions test local test

--- truffle-config.js
+++ truffle-config.js
@@ -45,11 +45,12 @@ module.exports = {
     // You should run a client (like ganache-cli, geth or parity) in a separate terminal
     // tab if you use this network and you must also set the `host`, `port` and `network_id`
     // options below to some value.
-    // development: {
-    //   host: "127.0.0.1", // Localhost (default: none)
-    //   port: 8545, // Standard Ethereum port (default: none)
-    //   network_id: "*", // Any network (default: none)
-    // },
+    test: {
+      host: "127.0.0.1", // Localhost (default: none)
+      port: 8545, // Standard Ethereum port (default: none)
+      network_id: "*", // Any network (default: none)
+      disableConfirmationListener: true,
+    },
     mainnet_fork: {
       host: "127.0.0.1", // Localhost (default: none)
       port: 8545, // Standard Ethereum port (default: none)
