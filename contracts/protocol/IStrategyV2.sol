a8a5f9f31d5ddf95549cbcc86b99f78a2f6426cc
t4sk <tsk.nakamura@gmail.com>
2021-01-26 09:36:50 +0900

    IStrategyV2: fix comments

--- contracts/protocol/IStrategyV2.sol
+++ contracts/protocol/IStrategyV2.sol
@@ -45,14 +45,14 @@ interface IStrategyV2 {
     function totalAssets() external view returns (uint);
 
     /*
-    @notice Deposit `amount` underlying token for yield token
+    @notice Deposit `amount` underlying token
     @param amount Amount of underlying token to deposit
     */
     function deposit(uint _amount) external;
 
     /*
-    @notice Withdraw `amount` yield token to withdraw
-    @param amount Amount of yield token to withdraw
+    @notice Withdraw `_amount` underlying token
+    @param amount Amount of underlying token to withdraw
     */
     function withdraw(uint _amount) external;
 
