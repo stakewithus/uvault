7d8a90b05b6c34986c9875f180b08b69db5c35e1
t4sk <tsk.nakamura@gmail.com>
2021-01-27 13:36:46 +0900

    StrategyEth test

---
+++ contracts/tests/StrategyETHTest.sol
@@ -0,0 +1,38 @@
+// SPDX-License-Identifier: AGPL-3.0-or-later
+pragma solidity 0.6.11;
+
+import "../StrategyETH.sol";
+import "./TestToken.sol";
+
+/* solium-disable */
+contract StrategyETHTest is StrategyETH {
+    constructor(
+        address _controller,
+        address _vault,
+    ) public StrategyETH(_controller, _vault) {}
+
+    receive() external payable {}
+
+    function _totalAssets() internal view override returns (uint) {
+        return address(this).balance;
+    }
+
+    function _depositEth() internal override {}
+
+    function _getTotalShares() internal view override returns (uint) {
+        return address(this).balance;
+    }
+
+    function _withdrawEth(uint _shares) internal override {}
+
+    function harvest() external override onlyAuthorized {}
+
+    function exit() external override onlyAuthorized {
+        _withdrawAll();
+    }
+
+    function sweep(address _token) external override onlyAdmin {
+        require(_token != underlying, "protected token");
+        IERC20(_token).safeTransfer(admin, IERC20(_token).balanceOf(address(this)));
+    }
+}
