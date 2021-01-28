fbbf073fd6b355c0fab3c2a9fba7b56e5e5f6dee
t4sk <tsk.nakamura@gmail.com>
2021-01-28 10:40:45 +0900

    fix StrategyNoOpETH

--- contracts/strategies/StrategyNoOpETH.sol
+++ contracts/strategies/StrategyNoOpETH.sol
@@ -5,6 +5,8 @@ pragma solidity 0.6.11;
 version 1.2.0
 */
 
+import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
+import "@openzeppelin/contracts/token/ERC20/SafeERC20.sol";
 import "../protocol/IStrategyETH.sol";
 
 /*
