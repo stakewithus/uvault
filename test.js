ed299a02ef73d6ea235380fecce49701ce5ad5b5
t4sk <tsk.nakamura@gmail.com>
2021-02-02 08:45:46 +0900

    fix test script

--- test.js
+++ test.js
@@ -1,4 +1,3 @@
-const glob = require("glob")
 const child_process = require("child_process")
 const util = require("util")
 
@@ -8,27 +7,12 @@ function sleep(ms) {
   return new Promise((resolve) => setTimeout(resolve, ms))
 }
 
-function _glob(pattern) {
-  return new Promise((resolve, reject) => {
-    glob(pattern, {}, (err, files) => {
-      if (err) {
-        reject(err)
-      } else {
-        resolve(files)
-      }
-    })
-  })
-}
-
-const PATTERN = "test/mainnet/StrategyAave*/test-*.ts"
-
 async function main() {
-  // test/mainnet/**/test-*.ts
-  // const pattern = process.argv[2].trim()
   console.log(`Running mainnet tests...`)
 
-  // BUG passing pattern from input does not return correct files
-  const files = await _glob(PATTERN)
+  // this will be expaned to list of files
+  // test/mainnet/**/test-*.ts
+  const files = process.argv.slice(2)
 
   for (const file of files) {
     console.log(file)
