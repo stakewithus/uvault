24e535478895323fd8872e201d1cb8eaa5eb0754
t4sk <tsk.nakamura@gmail.com>
2021-02-01 12:15:02 +0900

    reorder stable coin vars

--- contracts/strategies/StrategyBusdV2.sol
+++ contracts/strategies/StrategyBusdV2.sol
@@ -13,10 +13,10 @@ contract StrategyBusdV2 is StrategyERC20 {
     address private constant UNISWAP = 0x7a250d5630B4cF539739dF2C5dAcb4c659F2488D;
     address private constant WETH = 0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2;
 
-    address private constant BUSD = 0x4Fabb145d64652a948d72533023f6E7A623C7C53;
     address internal constant DAI = 0x6B175474E89094C44Da98b954EedeAC495271d0F;
     address internal constant USDC = 0xA0b86991c6218b36c1d19D4a2e9Eb0cE3606eB48;
     address internal constant USDT = 0xdAC17F958D2ee523a2206206994597C13D831ec7;
+    address internal constant BUSD = 0x4Fabb145d64652a948d72533023f6E7A623C7C53;
 
     // DAI = 0 | USDC = 1 | USDT = 2 | BUSD = 3
     uint internal underlyingIndex;
