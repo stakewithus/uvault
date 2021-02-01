60f180939ff1e3a638bfcf79e4d732e1b960bf71
t4sk <tsk.nakamura@gmail.com>
2021-02-01 12:27:17 +0900

    config busd address

--- scripts/config.ts
+++ scripts/config.ts
@@ -54,6 +54,7 @@ export interface Config {
     dai: string
     usdc: string
     usdt: string
+    busd: string
   }
   // mainnet dev
   dev: {
@@ -85,6 +86,7 @@ export interface Config {
     dai: string
     usdc: string
     usdt: string
+    busd: string
   }
 }
 
@@ -151,6 +153,7 @@ const config: Config = {
     dai: "0x6B175474E89094C44Da98b954EedeAC495271d0F",
     usdc: "0xA0b86991c6218b36c1d19D4a2e9Eb0cE3606eB48",
     usdt: "0xdAC17F958D2ee523a2206206994597C13D831ec7",
+    busd: "0x4Fabb145d64652a948d72533023f6E7A623C7C53",
   },
   // mainnet dev
   dev: {
@@ -188,6 +191,7 @@ const config: Config = {
     dai: "0x6B175474E89094C44Da98b954EedeAC495271d0F",
     usdc: "0xA0b86991c6218b36c1d19D4a2e9Eb0cE3606eB48",
     usdt: "0xdAC17F958D2ee523a2206206994597C13D831ec7",
+    busd: "0x4Fabb145d64652a948d72533023f6E7A623C7C53",
   },
 }
 
