a114ff464da52a59268b30e5e220d244803a7dbb
t4sk <tsk.nakamura@gmail.com>
2021-02-03 14:06:14 +0900

    strategy gusd gusd

---
+++ contracts/strategies/StrategyGusdGusdV2.sol
@@ -0,0 +1,13 @@
+// SPDX-License-Identifier: AGPL-3.0-or-later
+pragma solidity 0.6.11;
+
+import "./StrategyGusdV2.sol";
+
+contract StrategyGusdGusdV2 is StrategyGusdV2 {
+    constructor(address _controller, address _vault)
+        public
+        StrategyGusdV2(_controller, _vault, GUSD)
+    {
+        underlyingIndex = 0;
+    }
+}
