6a8b4f6a9b4f212cc3fc44ce18096a3c2d59298a
t4sk <tsk.nakamura@gmail.com>
2021-02-04 11:37:54 +0900

    update strategy bbtc

--- contracts/strategies/StrategyBbtc.sol
+++ contracts/strategies/StrategyBbtc.sol
@@ -181,7 +181,8 @@ contract StrategyBbtc is StrategyERC20 {
     }
 
     function _claimRewards(address _token) private {
-        LiquidityGaugeV2(GAUGE).claim_rewards();
+        // BBTC gauge has no rewards so don't call
+        // LiquidityGaugeV2(GAUGE).claim_rewards();
         Minter(MINTER).mint(GAUGE);
 
         uint crvBal = IERC20(CRV).balanceOf(address(this));
