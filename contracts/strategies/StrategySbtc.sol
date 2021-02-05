065a6306ed1a72b1c47322f617a13ced5817b9dd
t4sk <tsk.nakamura@gmail.com>
2021-02-05 14:33:36 +0900

    remove todo

--- contracts/strategies/StrategySbtc.sol
+++ contracts/strategies/StrategySbtc.sol
@@ -166,7 +166,6 @@ contract StrategySbtc is StrategyERC20 {
     }
 
     function _claimRewards(address _token) private {
-        // TODO: claim BPT
         LiquidityGaugeReward(GAUGE).claim_rewards();
         // claim CRV
         Minter(MINTER).mint(GAUGE);
